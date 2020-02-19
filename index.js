c35754de0ba05629de337a7697fd4b87fd793c3f
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-19 15:08:34 +0100

    It works
    With Giphy API! Check it out on my Discord

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, giphyToken } = require('./config.json');
 const client = new Discord.Client();
+var GphApiClient = require('giphy-js-sdk-core')
+giffy = GphApiClient(giphyToken)
 
 client.once('ready', () => {
     console.log('Ready!')
@@ -8,13 +10,21 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if(message.member.hasPermission(["KICK_MEMBERS","BAN_MEMBERS"])) {
-
         if(message.content.startsWith(`${prefix}kick`)) {
-    
             let member = message.mentions.members.first();
             member.kick().then((member) => {
-                message.channel.send("You gotta kicked by Jackie " + member.displayName)
-            })
+                giffy.search('gifs', {"q": "fail"})
+                    .then((response) => {
+                        var totalReponses = response.data.length;
+                        var responseIndex = Math.floor((Math.random() * 10) +1) % totalReponses;
+                        var responseFinal = response.data[responseIndex];
+
+                        message.channel.send(":wave: " + member.displayName + " You are kicked by my ass!", {
+                            files: [responseFinal.images.fixed_height.url]
+                        })
+                    })
+                }
+            )
         }
 
     }
